7d62f641fe6352bbdf49059a7d4d311eb759705e
adorablenewbie <youngjong2965@gmail.com>
2025-08-27 19:36:47 +0900

    [ feat ] 에너미 제작

--- Assets/Scripts/Player/PlayerAnimationData.cs
+++ Assets/Scripts/Player/PlayerAnimationData.cs
@@ -15,6 +15,7 @@ public class PlayerAnimationData
 
     [SerializeField] private string attackParameterName = "@Attack";
     [SerializeField] private string comboAttackParameterName = "ComboAttack";
+    [SerializeField] private string baseAttackParameterName = "BaseAttack";
 
     public int GroundParameterHash { get; private set; }
     public int IdleParameterHash { get; private set; }
@@ -27,6 +28,7 @@ public class PlayerAnimationData
 
     public int AttackParameterHash { get; private set; }
     public int ComboAttackParameterHash { get; private set; }
+    public int BaseAttackParameterHash { get; private set; }
 
     public void Initialize()
     {
@@ -41,5 +43,6 @@ public class PlayerAnimationData
 
         AttackParameterHash = Animator.StringToHash(attackParameterName);
         ComboAttackParameterHash = Animator.StringToHash(comboAttackParameterName);
+        BaseAttackParameterHash = Animator.StringToHash(baseAttackParameterName);
     }
 }
